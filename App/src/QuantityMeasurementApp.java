24d9e2b11d37d15530e282cb2097c1f9c8fbbf1f
rkhan777737 <rkhan777737@gmail.com>
2026-05-05 17:44:05 +0530

    uc11

--- App/src/QuantityMeasurementApp.java
+++ App/src/QuantityMeasurementApp.java
@@ -2,15 +2,13 @@ package App.src;
 
 import java.util.Objects;
 
-// UC10: Interface must be public to be accessed by the test folder
-interface IMeasurable {
+public interface IMeasurable {
     double convertToBaseUnit(double value);
     double convertFromBaseUnit(double baseValue);
     String getUnitName();
 }
 
-// UC10: Generic Quantity class must be public
-class Quantity<U extends IMeasurable> {
+public class Quantity<U extends IMeasurable> {
     private final double value;
     private final U unit;
 
@@ -20,17 +18,31 @@ class Quantity<U extends IMeasurable> {
         this.unit = unit;
     }
 
+    // UC11: Generic Addition
+    public Quantity<U> add(Quantity<U> that) {
+        return add(that, this.unit);
+    }
+
+    public Quantity<U> add(Quantity<U> that, U targetUnit) {
+        double sumInBase = this.unit.convertToBaseUnit(this.value) +
+                that.unit.convertToBaseUnit(that.value);
+        return new Quantity<>(targetUnit.convertFromBaseUnit(sumInBase), targetUnit);
+    }
+
+    // UC11: Generic Conversion
+    public Quantity<U> convertTo(U targetUnit) {
+        double baseValue = this.unit.convertToBaseUnit(this.value);
+        return new Quantity<>(targetUnit.convertFromBaseUnit(baseValue), targetUnit);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Quantity<?> that = (Quantity<?>) o;
-
-        // UC10: Category safety check (prevents comparing Length to Weight)
-        if (this.unit.getClass() != that.unit.getClass()) return false;
-
+        if (this.unit.getClass() != that.unit.getClass()) return false; // Category Safety
         return Math.abs(this.unit.convertToBaseUnit(this.value) -
-                ((IMeasurable)that.unit).convertToBaseUnit(((Quantity)o).value)) < 1e-6;
+                ((IMeasurable)that.unit).convertToBaseUnit(((Quantity<?>)o).value)) < 1e-6;
     }
 
     @Override
@@ -39,21 +51,19 @@ class Quantity<U extends IMeasurable> {
     }
 }
 
-// These can stay in the same file as long as they aren't marked 'public'
-enum LengthUnit implements IMeasurable {
-    FEET(1.0), INCHES(1.0/12.0);
-    private final double factor;
-    LengthUnit(double factor) { this.factor = factor; }
-    public double convertToBaseUnit(double v) { return v * factor; }
-    public double convertFromBaseUnit(double b) { return b / factor; }
-    public String getUnitName() { return this.name(); }
-}
+// UC11: New Volume Category
+public enum VolumeUnit implements IMeasurable {
+    LITRE(1.0),
+    MILLILITRE(0.001),
+    GALLON(3.78541);
 
-enum WeightUnit implements IMeasurable {
-    KILOGRAM(1.0), GRAM(0.001);
     private final double factor;
-    WeightUnit(double factor) { this.factor = factor; }
+    VolumeUnit(double factor) { this.factor = factor; }
+
+    @Override
     public double convertToBaseUnit(double v) { return v * factor; }
+    @Override
     public double convertFromBaseUnit(double b) { return b / factor; }
+    @Override
     public String getUnitName() { return this.name(); }
 }
